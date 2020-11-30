c853c41b9d7b03e1f7eb2653b226b6a7d75282dd
Jason Todd <jake@lltodd.family>
2020-11-30 12:51:52 -0700

    Formatting

--- weather.sh
+++ weather.sh
@@ -70,42 +70,42 @@ mv wwa.txt _includes
 $wxcast forecast $zipcode > 7dayfcast.txt
 sed -i 's/No forecast found for location\: 85630 coordinates\: 31\.902220000000057\,\-110\.21934499999998/The NWS has no forecast available to download for Saint David\./' 7dayfcast.txt
 sed -i 's/$/<br>/' 7dayfcast.txt
-#sed -i 's/\:/<b>\:<\/b>/' 7dayfcast.txt
-#sed -i 's/Today/<b>Today<\/b>/' 7dayfcast.txt
-#sed -i 's/Tonight/<b>Tonight<\/b>/' 7dayfcast.txt
-#sed -i 's/Tomorrow/<b>Tomorrow<\/b>/' 7dayfcast.txt
-#sed -i 's/This/<b>This<\/b>/' 7dayfcast.txt
-#sed -i 's/Thru/<b>Thru<\/b>/' 7dayfcast.txt
-#sed -i 's/Afternoon/<b>Afternoon<\/b>/' 7dayfcast.txt
-#sed -i 's/Evening/<b>Evening<\/b>/' 7dayfcast.txt
-#sed -i 's/Overnight/<b>Overnight<\/b>/' 7dayfcast.txt
-#sed -i 's/Day/<b>Day<\/b>/' 7dayfcast.txt
-#sed -i 's/Night/<b>Night<\/b>/' 7dayfcast.txt
-#sed -i 's/Eve/<b>Eve<\/b>/' 7dayfcast.txt
-#sed -i 's/Next/<b>Next<\/b>/' 7dayfcast.txt
-#sed -i 's/Weekend/<b>Weekend<\/b>/' 7dayfcast.txt
-#sed -i 's/Week/<b>Week<\/b>/' 7dayfcast.txt
-#sed -i 's/VeteransDay/<b>Veterans Day<\/b/' 7dayfcast.txt
-#sed -i 's/Memorial/<b>Memorial<\/b/' 7dayfcast.txt
-#sed -i 's/Labor/<b>Labor<\/b/' 7dayfcast.txt
-#sed -i 's/Columbus/<b>Columbus<\/b/' 7dayfcast.txt
-#sed -i 's/Presidents/<b>Presidents<\/b/' 7dayfcast.txt
-#sed -i 's/New Years/<b>New Years<\/b/' 7dayfcast.txt
-#sed -i 's/Independence/<b>Independence<\/b/' 7dayfcast.txt
-#sed -i 's/Inauguration/<b>Inauguration<\/b/' 7dayfcast.txt
-#sed -i 's/Valentines/<b>Valentines<\/b/' 7dayfcast.txt
-#sed -i 's/New Year/<b>New Year<\/b/' 7dayfcast.txt
-#sed -i 's/Halloween/<b>Halloween<\/b/' 7dayfcast.txt
-#sed -i 's/Easter/<b>Easter<\/b/' 7dayfcast.txt
-#sed -i 's/Thanksgiving/<b>Thanksgiving<\/b/' 7dayfcast.txt
-#sed -i 's/Christmas/<b>Christmas<\/b/' 7dayfcast.txt
-#sed -i 's/Monday/<br><b>Monday<\/b>/' 7dayfcast.txt
-#sed -i 's/Tuesday/<br><b>Tuesday<\/b>/' 7dayfcast.txt
-#sed -i 's/Wednesday/<br><b>Wednesday<\/b>/' 7dayfcast.txt
-#sed -i 's/Thursday/<br><b>Thursday<\/b>/' 7dayfcast.txt
-#sed -i 's/Friday/<br><b>Friday<\/b>/' 7dayfcast.txt
-#sed -i 's/Saturday/<br><b>Saturday<\/b>/' 7dayfcast.txt
-#sed -i 's/Sunday/<br><b>Sunday<\/b>/' 7dayfcast.txt
+sed -i 's/\:/<b>\:<\/b>/' 7dayfcast.txt
+sed -i 's/Today/<b>Today<\/b>/' 7dayfcast.txt
+sed -i 's/Tonight/<b>Tonight<\/b>/' 7dayfcast.txt
+sed -i 's/Tomorrow/<b>Tomorrow<\/b>/' 7dayfcast.txt
+sed -i 's/This/<b>This<\/b>/' 7dayfcast.txt
+sed -i 's/Thru/<b>Thru<\/b>/' 7dayfcast.txt
+sed -i 's/Afternoon/<b>Afternoon<\/b>/' 7dayfcast.txt
+sed -i 's/Evening/<b>Evening<\/b>/' 7dayfcast.txt
+sed -i 's/Overnight/<b>Overnight<\/b>/' 7dayfcast.txt
+sed -i 's/Day/<b>Day<\/b>/' 7dayfcast.txt
+sed -i 's/Night/<b>Night<\/b>/' 7dayfcast.txt
+sed -i 's/Eve/<b>Eve<\/b>/' 7dayfcast.txt
+sed -i 's/Next/<b>Next<\/b>/' 7dayfcast.txt
+sed -i 's/Weekend/<b>Weekend<\/b>/' 7dayfcast.txt
+sed -i 's/Week/<b>Week<\/b>/' 7dayfcast.txt
+sed -i 's/VeteransDay/<b>Veterans Day<\/b/' 7dayfcast.txt
+sed -i 's/Memorial/<b>Memorial<\/b/' 7dayfcast.txt
+sed -i 's/Labor/<b>Labor<\/b/' 7dayfcast.txt
+sed -i 's/Columbus/<b>Columbus<\/b/' 7dayfcast.txt
+sed -i 's/Presidents/<b>Presidents<\/b/' 7dayfcast.txt
+sed -i 's/New Years/<b>New Years<\/b/' 7dayfcast.txt
+sed -i 's/Independence/<b>Independence<\/b/' 7dayfcast.txt
+sed -i 's/Inauguration/<b>Inauguration<\/b/' 7dayfcast.txt
+sed -i 's/Valentines/<b>Valentines<\/b/' 7dayfcast.txt
+sed -i 's/New Year/<b>New Year<\/b/' 7dayfcast.txt
+sed -i 's/Halloween/<b>Halloween<\/b/' 7dayfcast.txt
+sed -i 's/Easter/<b>Easter<\/b/' 7dayfcast.txt
+sed -i 's/Thanksgiving/<b>Thanksgiving<\/b/' 7dayfcast.txt
+sed -i 's/Christmas/<b>Christmas<\/b/' 7dayfcast.txt
+sed -i 's/Monday/<br><b>Monday<\/b>/' 7dayfcast.txt
+sed -i 's/Tuesday/<br><b>Tuesday<\/b>/' 7dayfcast.txt
+sed -i 's/Wednesday/<br><b>Wednesday<\/b>/' 7dayfcast.txt
+sed -i 's/Thursday/<br><b>Thursday<\/b>/' 7dayfcast.txt
+sed -i 's/Friday/<br><b>Friday<\/b>/' 7dayfcast.txt
+sed -i 's/Saturday/<br><b>Saturday<\/b>/' 7dayfcast.txt
+sed -i 's/Sunday/<br><b>Sunday<\/b>/' 7dayfcast.txt
 mv 7dayfcast.txt _includes
 ##########################################################
 
